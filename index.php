17e16a876fe00a4f9c9b30bd00904ea46b86a87c
Talha Sajjad <talha.sajjad.software@gmail.com>
2024-07-09 01:30:13 +0500

    formatted

--- index.php
+++ index.php
@@ -6,17 +6,20 @@ $quote = json_decode(file_get_contents($api_url));
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Random Inspirational Quote</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <div class="quote"><?php echo $quote->content;?></div>
-    <div class="author"> By-<?php echo $quote->author;?></div>
+    <div class="quote"><?php echo $quote->content; ?></div>
+    <div class="author"> By-<?php echo $quote->author; ?></div>
     <form method="post">
         <button type="submit" class="button">New Quote</button>
     </form>
 </body>
+
 </html>
